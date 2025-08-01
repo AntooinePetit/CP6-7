2e0e36e1c22d98c15bfbe00b01c2684b940c85df
Antoine Petit <antooine.petit@gmail.com>
2025-08-01 15:58:06 +0200

    docs: Add initial project documentation
    - Added database schema and setup instructions
    - Included initial README with project info
    - Added basic index page links

--- index.php
+++ index.php
@@ -13,7 +13,13 @@ session_start();
   <?php include_once 'components/header.php'; ?>
 
   <main>
-
+    <h1>Bienvenue sur le site Esports</h1>
+    <div class="info">
+      <a href="teams/list.php">Découvrez nos équipe</a><br>
+      <a href="tournaments/list.php">Découvrez nos tournois</a><br>
+      <a href="users/signup.php">Créer un compte</a><br>
+      <a href="users/login.php">Se connecter</a>
+    </div>
   </main>
 
   <?php include_once 'components/footer.php'; ?>
